75a4a3989fbd3090650e6cc25a49629cb610336d
Sivasankari26 <68770639+Sivasankari26@users.noreply.github.com>
2020-08-07 11:49:33 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,32 @@
+
+
+from flask_sqlalchemy import SQLAlchemy 
+
+from app import db                                         
+
+
+class Employee(db.Model):
+    __tablename__ = 'employee'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    age = db.Column(db.String())
+    address = db.Column(db.String())
+
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'name': self.name,
+            'age': self.age,
+            'address':self.address
+        }
+
+
